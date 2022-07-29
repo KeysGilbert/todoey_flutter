1d1c326c2c7c89976abcd42dcf6c0b7ac2d8de05
KeysGilbert <gbk5070@psu.edu>
2022-07-29 12:47:20 -0400

    adjust padding

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -7,40 +7,40 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10.0),
-                topRight: Radius.circular(10.0))),
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0))),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                'Add Task',
-                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 45.0),
-              ),
+            Text(
+              'Add Task',
+              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 45.0),
             ),
             TextField(
               decoration: InputDecoration(
                   border: UnderlineInputBorder(
                       borderSide: BorderSide(
                           color: Colors.lightBlueAccent, width: 15.0))),
+              autofocus: true,
+              textAlign: TextAlign.center,
               onChanged: null,
             ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: 250.0,
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.lightBlueAccent), ),
-                    onPressed: null,
-                    child: Text(
-                      'Add',
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    )),
-              ),
+            SizedBox(
+              width: 250.0,
+              child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.lightBlueAccent),
+                  ),
+                  onPressed: null,
+                  child: Text(
+                    'Add',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  )),
             )
           ],
         ),
