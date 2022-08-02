9772f7f5f93a4db576e72f8cd111ea8d8c17615b
KeysGilbert <gbk5070@psu.edu>
2022-08-02 13:18:59 -0400

    convert checkbox to stateful widget

--- lib/task_tile.dart
+++ lib/task_tile.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TaskCheckbox();
+  }
+}
 
+class TaskCheckbox extends StatefulWidget {
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool? isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
-      value: false,
-      onChanged: null,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
       title: Text("Placeholder 1"),
       controlAffinity: ListTileControlAffinity.trailing,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
