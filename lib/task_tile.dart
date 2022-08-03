07598e437a704d649b458d863476cf33730680a2
KeysGilbert <gbk5070@psu.edu>
2022-08-03 12:53:22 -0400

    keep track of checkbox state

--- lib/task_tile.dart
+++ lib/task_tile.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
-    return TaskCheckbox();
+    return TaskCheckbox(isChecked);
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
+class TaskCheckbox extends StatelessWidget {
   @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
 
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool? isChecked = false;
+  final bool checkboxState;
+  TaskCheckbox(this.checkboxState);
 
-  @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
-      value: isChecked,
+      value: checkboxState,
       onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue;
-        });
+   //     setState(() {
+     //     isChecked = newValue;
+       // });
       },
-      title: Text("Placeholder 1"),
+      title: Text("Placeholder 1", style: TextStyle(decoration: checkboxState? TextDecoration.lineThrough : null),),
       controlAffinity: ListTileControlAffinity.trailing,
       activeColor: Colors.lightBlueAccent,
     );
