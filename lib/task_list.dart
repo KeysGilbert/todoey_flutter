6c7adc96945c99855a07cc2ee73733e162765726
KeysGilbert <gbk5070@psu.edu>
2022-08-09 13:06:15 -0400

    convert to stateful

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
+import 'task.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [];
+  
   @override
   Widget build(BuildContext context) {
     return ListView(children: <Widget>[
