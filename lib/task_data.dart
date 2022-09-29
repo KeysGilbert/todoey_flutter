8903428b7a31e2287a4bcadeb4e144920c3b6892
KeysGilbert <gbk5070@psu.edu>
2022-09-29 13:28:08 -0400

    function to mark task as done

--- lib/task_data.dart
+++ lib/task_data.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/cupertino.dart';
-
 import 'package:todoey_flutter/task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
-
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(taskText: "practice coding"),
     Task(taskText: "Apply for jobs"),
     Task(taskText: "Watch youtube")
   ];
 
-  int get taskCount => tasks.length;
+  int get taskCount => _tasks.length;
+  UnmodifiableListView<Task> get taskInfo => UnmodifiableListView(
+      _tasks); //do not want to unnecesarrily change tasks list
 
   void addTask(String taskText) {
-    tasks.add(Task(taskText: taskText));
+    _tasks.add(Task(taskText: taskText));
+    notifyListeners(); //notify widget that data being provided has changed
+  }
+
+  void crossOutTask(Task task) {
+    task.finish();
     notifyListeners();
   }
 }
