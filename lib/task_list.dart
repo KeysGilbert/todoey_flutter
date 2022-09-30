8ed6ee9c203291aab39dd489811503e9550a24f3
KeysGilbert <gbk5070@psu.edu>
2022-09-30 13:05:44 -0400

    use Comsumer, checkbox now works again

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -4,23 +4,24 @@ import 'task.dart';
 import 'package:provider/provider.dart';
 import 'task_data.dart';
 
-
 class TaskList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: Provider.of<TaskData>(context).taskCount,
-        itemBuilder: ((context, index) {
-          return TaskTile(
-            taskTitle: Provider.of<TaskData>(context).tasks[index].taskText,
-            isChecked: Provider.of<TaskData>(context).tasks[index].isFinished,
-            checkboxCallback: (checkboxState) {
-       //       setState(() {
-       //         widget.tasks[index].finish();
-        //      });
-            },
-          );
-        }));
+
+    //obtain Provider type fromm ancestor and pass to builder
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+          itemCount: Provider.of<TaskData>(context).taskCount,
+          itemBuilder: ((context, index) {
+            return TaskTile(
+              taskTitle: taskData.taskInfo[index].taskText,
+              isChecked: taskData.taskInfo[index].isFinished,
+              checkboxCallback: (checkboxState) {
+                //taskData.crossOutTask(taskData.taskInfo[index]);
+                taskData.crossOutTask(taskData.taskInfo[index]);
+              },
+            );
+          }));
+    });
   }
 }
